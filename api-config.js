6166ccea10ec88b76d57ed66e18c0aac8d5d812e
Anderson de Paula <devorgweb@gmail.com>
2026-05-23 10:43:38 -0300

    Update api-config.js
    direcionamento

--- api-config.js
+++ api-config.js
@@ -2,6 +2,6 @@ window.ZAITPAY_API_CONFIG = {
   BASE_URL: "https://api.zaitpay.com.br",
   // deixe vazio se seu backend responde direto na raiz.
   // use "api" se suas rotas forem https://api.zaitpay.com.br/api/...
-  PREFIX: "",
+  PREFIX: "api",
   TOKEN_KEY: "zaitpay_access_token"
 };
